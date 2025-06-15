d4acba6af82785f38bc129fb0b64429576e63976
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-06-16 03:01:40 +0330

    Signatures for BETH

--- src/BETH.sol
+++ src/BETH.sol
@@ -4,15 +4,35 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BETH is ERC20 {
-    uint256 public number;
+    mapping(uint256 => bool) nullifiers;
+    mapping(uint256 => bool) coins;
 
     constructor() ERC20("Burnt ETH", "BETH") {}
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    function mintCoin(
+        uint256[2] calldata _pA,
+        uint256[2][2] calldata _pB,
+        uint256[2] calldata _pC,
+        uint256 _blockNumber,
+        uint256 _nullifier,
+        uint256 _encryptedBalance,
+        uint256 _fee
+    ) public {
+        require(!nullifiers[_nullifier]);
+        require(!coins[_encryptedBalance]);
+        bytes32 blockRoot = blockhash(_blockNumber);
+        uint256 commitment = uint256(keccak256(abi.encodePacked(blockRoot, _nullifier, _encryptedBalance)));
+        commitment = commitment << 8 >> 8;
+        _mint(msg.sender, _fee);
+        nullifiers[_nullifier] = true;
+        coins[_encryptedBalance] = true;
     }
 
-    function increment() public {
-        number++;
+    function spendCoin(uint256 _coin, uint256 _amount, uint256 _remainingCoin) public {
+        require(coins[_coin]);
+        require(!coins[_remainingCoin]);
+        _mint(msg.sender, _amount);
+        coins[_coin] = false;
+        coins[_remainingCoin] = true;
     }
 }
