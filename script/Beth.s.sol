532f011b7f71323a5a62e88b9f35481ef7b832d4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-07-09 15:47:08 +0330

    Accept solidity verifiers in BETH contract

--- script/Beth.s.sol
+++ script/Beth.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {BETH} from "../src/BETH.sol";
+import {BETH, Verifier} from "../src/BETH.sol";
 
 contract BETHScript is Script {
     BETH public beth;
@@ -12,7 +12,7 @@ contract BETHScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        beth = new BETH();
+        beth = new BETH(Verifier(address(0)), Verifier(address(0)));
 
         vm.stopBroadcast();
     }
--- src/BETH.sol
+++ src/BETH.sol
@@ -3,11 +3,25 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+interface Verifier {
+    function verifyProof(
+        uint256[2] calldata _pA,
+        uint256[2][2] calldata _pB,
+        uint256[2] calldata _pC,
+        uint256[1] calldata _pubSignals
+    ) external view returns (bool);
+}
+
 contract BETH is ERC20 {
+    Verifier proofOfBurnVerifier;
+    Verifier spendVerifier;
     mapping(uint256 => bool) nullifiers;
     mapping(uint256 => bool) coins;
 
-    constructor() ERC20("Burnt ETH", "BETH") {}
+    constructor(Verifier _proofOfBurnVerifier, Verifier _spendVerifier) ERC20("Burnt ETH", "BETH") {
+        proofOfBurnVerifier = _proofOfBurnVerifier;
+        spendVerifier = _spendVerifier;
+    }
 
     function mintCoin(
         uint256[2] calldata _pA,
@@ -15,17 +29,25 @@ contract BETH is ERC20 {
         uint256[2] calldata _pC,
         uint256 _blockNumber,
         uint256 _nullifier,
-        uint256 _encryptedBalance,
-        uint256 _fee
+        uint256 _remainingCoin,
+        uint256 _fee,
+        uint256 _spend,
+        address _receiver
     ) public {
+        require(_fee + _spend <= 1 ether); // Mint cap
         require(!nullifiers[_nullifier]);
-        require(!coins[_encryptedBalance]);
+        require(!coins[_remainingCoin]);
         bytes32 blockRoot = blockhash(_blockNumber);
-        uint256 commitment = uint256(keccak256(abi.encodePacked(blockRoot, _nullifier, _encryptedBalance)));
-        commitment = commitment << 8 >> 8;
+        uint256 commitment = uint256(
+            keccak256(
+                abi.encodePacked(blockRoot, _nullifier, _remainingCoin, _fee, _spend, uint256(uint160(_receiver)))
+            )
+        ) >> 8;
+        require(proofOfBurnVerifier.verifyProof(_pA, _pB, _pC, [commitment]), "Invalid proof!");
         _mint(msg.sender, _fee);
+        _mint(_receiver, _spend);
         nullifiers[_nullifier] = true;
-        coins[_encryptedBalance] = true;
+        coins[_remainingCoin] = true;
     }
 
     function spendCoin(uint256 _coin, uint256 _amount, uint256 _remainingCoin) public {
