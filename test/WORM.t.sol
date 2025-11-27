cf7760e4e8c301470243f66ef00c01ec8a97d3bd
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-11-27 19:13:03 +0330

    test: Test sum-rewards

--- test/WORM.t.sol
+++ test/WORM.t.sol
@@ -53,5 +53,10 @@ contract WORMTest is Test {
         vm.warp(ts + 600 * 10093127);
         assertEq(worm.currentEpoch(), 10093127);
         assertEq(worm.currentReward(), 0);
+        uint256 sumRewards = 0;
+        for (uint256 i = 0; i < 10093127; i++) {
+            sumRewards += worm.cachedReward(i);
+        }
+        assertEq(sumRewards, 15148322929356316021917252);
     }
 }
