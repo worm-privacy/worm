658f4cc09bdb043c20c7e67dd34c14bbda0a3076
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-12-06 18:17:52 +0330

    feat: DeployAnvil script

---
+++ script/DeployAnvil.s.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {BETH} from "../src/BETH.sol";
+import {WORM} from "../src/WORM.sol";
+import {Staking} from "../src/Staking.sol";
+import {Genesis} from "../src/Genesis.sol";
+import {ProofOfBurnVerifier} from "../src/ProofOfBurnVerifier.sol";
+import {SpendVerifier} from "../src/SpendVerifier.sol";
+import {IVerifier} from "../src/interfaces/IVerifier.sol";
+import {IRewardPool} from "../src/interfaces/IRewardPool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract FakePool {
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int256 amountSpecified,
+        uint160 sqrtPriceLimitX96,
+        bytes calldata data
+    ) external returns (int256 amount0, int256 amount1) {
+        console.log(recipient);
+        console.log(zeroForOne);
+        console.log(amountSpecified);
+        console.log(sqrtPriceLimitX96);
+        amount0 = 0;
+        amount1 = 0;
+    }
+}
+
+contract DeployAnvilScript is Script {
+    BETH public beth;
+    WORM public worm;
+    Staking public staking;
+
+    Genesis public communityGenesis;
+    Genesis public othersGenesis;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        IVerifier proofOfBurnVerifier = new ProofOfBurnVerifier();
+        IVerifier spendVeifier = new SpendVerifier();
+
+        beth = new BETH(proofOfBurnVerifier, spendVeifier, msg.sender, 0);
+        worm = new WORM(IERC20(beth), msg.sender, 0);
+        staking = new Staking(IERC20(worm), IERC20(beth));
+        beth.initRewardPool(IRewardPool(staking));
+
+        console.log("BETH", address(beth));
+        console.log("WORM", address(worm));
+        console.log("Staking", address(staking));
+        FakePool fakePool = new FakePool();
+        console.log("Fake pool", address(fakePool));
+
+        vm.stopBroadcast();
+    }
+}
