274b8f68d40d73e4e3606f3a781f6a01e0d3b580
Ali Ghahremani <alighahremani1377@gmail.com>
2026-02-16 17:05:51 +0330

    feat: deploy BETHToETH script added

---
+++ script/DeployBETHToETH.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/BETHToETH.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IWNativeToken  } from "src/interfaces/IWNativeToken.sol";
+
+contract DeployBETHToETH is Script {
+    // mainnet addresses
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant BETH = 0x5624344235607940d4d4EE76Bf8817d403EB9Cf8;
+    
+    function run() external {
+        vm.startBroadcast();
+        
+        BETHToETH bethToEth = new BETHToETH(IERC20(BETH),IWNativeToken(WETH));
+        
+        console.log("BETHToETH deployed to:", address(bethToEth));
+        console.log("BETH address:", BETH);
+        console.log("WETH address:", WETH);
+        
+        vm.stopBroadcast();
+    }
+}
