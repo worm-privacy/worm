9f85bbbf469e252257c3336a60690875d4e96231
Ali Ghahremani <alighahremani1377@gmail.com>
2026-02-16 17:34:55 +0330

    ref: forge fmt

--- src/hooks/cypher-eth/BETHToETH.sol
+++ src/hooks/cypher-eth/BETHToETH.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IWNativeToken  } from "src/hooks/cypher-eth/IWNativeToken.sol";
-import { ISwapRouter  } from "src/hooks/cypher-eth/ISwapRouter.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IWNativeToken} from "src/hooks/cypher-eth/IWNativeToken.sol";
+import {ISwapRouter} from "src/hooks/cypher-eth/ISwapRouter.sol";
 
-contract BETHToETH{
-
+contract BETHToETH {
     IERC20 public immutable bethContract;
     IWNativeToken public immutable wethContract;
 
@@ -17,31 +16,35 @@ contract BETHToETH{
         wethContract = _wethContract;
     }
 
-    function swapBethWithEth(uint256 _swapAmount, address _recipient, ISwapRouter _swapRouter) public returns (uint256 amountOut){
+    function swapBethWithEth(uint256 _swapAmount, address _recipient, ISwapRouter _swapRouter)
+        public
+        returns (uint256 amountOut)
+    {
         require(_swapAmount > 0, "Amount must be greater than 0");
         require(_recipient != address(0), "Invalid recipient");
 
         require(
-            bethContract.transferFrom(msg.sender, address(this), _swapAmount), 
-            "error while transferFrom beth to this"
+            bethContract.transferFrom(msg.sender, address(this), _swapAmount), "error while transferFrom beth to this"
         );
 
         bethContract.approve(address(_swapRouter), _swapAmount);
 
-        amountOut = _swapRouter.exactInputSingle(ISwapRouter.ExactInputSingleParams({
-            tokenIn: address(bethContract),
-            tokenOut: address(wethContract),
-            deployer: address(0),
-            recipient: address(this),
-            deadline: block.timestamp + 15 minutes,
-            amountIn: _swapAmount,
-            amountOutMinimum: 0,
-            limitSqrtPrice: 0
-        }));
+        amountOut = _swapRouter.exactInputSingle(
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(bethContract),
+                tokenOut: address(wethContract),
+                deployer: address(0),
+                recipient: address(this),
+                deadline: block.timestamp + 15 minutes,
+                amountIn: _swapAmount,
+                amountOutMinimum: 0,
+                limitSqrtPrice: 0
+            })
+        );
 
         bethContract.approve(address(_swapRouter), 0); // extra safety
         wethContract.withdraw(amountOut);
-        (bool success, ) = _recipient.call{value: amountOut}("");
+        (bool success,) = _recipient.call{value: amountOut}("");
         require(success, "ETH transfer failed");
     }
 
--- src/hooks/cypher-eth/ISwapRouter.sol
+++ src/hooks/cypher-eth/ISwapRouter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-        
-interface ISwapRouter{
+
+interface ISwapRouter {
     struct ExactInputSingleParams {
         address tokenIn;
         address tokenOut;
